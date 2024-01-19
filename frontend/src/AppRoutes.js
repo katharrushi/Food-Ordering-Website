fb061af6fed7784ec0589e00af91772e33ebdef0
katharrushi <rushikeshkathar384@gmail.com>
2024-01-19 23:21:52 +0530

    added new components

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import HomePage from './pages/Home/HomePage'
+import FoodPage from './pages/Food/FoodPage' 
 const AppRoutes = () => {
   return (
     <Routes>
     <Route path="/" element={<HomePage/>}/>
     <Route path="/search/:searchTerm" element={<HomePage/>}/>
     <Route path="/tag/:tag" element={<HomePage/>}/>
+    <Route path="/food/:id" element={<FoodPage/>}/> 
     </Routes>
   )
 }
