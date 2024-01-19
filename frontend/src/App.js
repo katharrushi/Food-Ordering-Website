aaee1ab7c624cb00f44c3d4e069fb8abd651dc50
katharrushi <rushikeshkathar384@gmail.com>
2024-01-19 22:43:20 +0530

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
-
+import AppRoutes from "./AppRoutes"
 function App() {
   return (
    <>
   <Header/>
+  <AppRoutes/>
    </>
   );
 }
---
+++ frontend/src/AppRoutes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import HomePage from './pages/Home/HomePage'
+const AppRoutes = () => {
+  return (
+    <Routes>
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/search/:searchTerm" element={<HomePage/>}/>
+    <Route path="/tag/:tag" element={<HomePage/>}/>
+    </Routes>
+  )
+}
+
+export default AppRoutes
