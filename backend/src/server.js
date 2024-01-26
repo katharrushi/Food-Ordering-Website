537c588dab3cb2cc8078425c8309e5f6228daae5
katharrushi <rushikeshkathar384@gmail.com>
2024-01-26 14:39:15 +0530

    publish on render.com

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import foodRouter from './routers/food.router.js'
 import userRouter from './routers/user.router.js'
 import orderRouter from './routers/order.router.js'
-
+import path, { dirname } from 'path';
 import { dbconnect } from './config/database.config.js';
 dbconnect();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app=express();
 app.use(express.json());
@@ -21,7 +25,15 @@ app.use(
 app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders',orderRouter);
-const PORT = 5000;
+const publicFolder = path.join(__dirname, 'public');
+app.use(express.static(publicFolder));
+
+app.get('*', (req, res) => {
+  const indexFilePath = path.join(publicFolder, 'index.html');
+  res.sendFile(indexFilePath);
+});
+
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, () => {
